7bdf72098cc1f6c338e0358684bdd97a2d1019ae
Imanov Khayal <khayalai@code.local>
2024-03-13 20:07:33 +0400

    first comment:fake folder

---
+++ 01.Introduction/script.js
@@ -0,0 +1,36 @@
+// let firstName = 'Xeyal'
+// console.log(firstName);
+// let lastName = 'Imanov'
+// console.log(lastName);
+// var age = 17
+// console.log(age);
+// const  uni = false
+// console.log(uni);
+// let adress = 'Bakixanov'
+// console.log(adress);
+
+
+// let firstName2 = 'Aqil'
+// console.log(firstName2);
+// let lastName2 = 'umalatov'
+// console.log(lastName2);
+// var age2 = 20
+// console.log(age2);
+// const  uni2 = false
+// console.log(uni2);
+// let adress2 = 'Nerimanov'
+// console.log(adress2);
+
+let a = 8
+
+let b = 9
+
+let c = '3'
+
+console.log(a-b);
+console.log(a+b);
+console.log(a+b+c);
+console.log(a+c+b);
+console.log(a/9);
+console.log(a*b);
+console.log(a*b*c);
---
+++ 02.Condition/script.js
@@ -0,0 +1,12 @@
+let userName = 'Xeyal'
+let surName = 'Imanov'
+let age = 17
+let isAdmin = true
+
+if (age >= 20) {console.log(1);
+    
+}
+else if (userName ==='Rufet' || userName === 'Azerin' || isAdmin === true) {console.log(2);
+    
+}
+else console.log('not found');
