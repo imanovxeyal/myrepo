c659295e8cc52e4732e8c157fb60ca0fbf649cc7
Imanov Khayal <khayalai@code.local>
2024-03-19 21:28:46 +0400

    Classwork

---
+++ 04.Array/script.js
@@ -0,0 +1,39 @@
+// const numbers = [5, 21, 14, 52, 17, 22]
+
+// let sum = 0
+// numbers.map(item => sum += item)
+// console.log(sum);
+
+// let sumIndex = 0
+// numbers.map( (_,index) => sumIndex += index)
+// console.log(sumIndex);
+//  console.log(sum - sumIndex);
+
+//  let sum2 = 1
+//  numbers.map(item => sum2 *= item)
+//  console.log(sum2);
+
+//  let sumIndex2 = 1
+//  numbers.map((_,index) => sumIndex2 *= index)
+//  console.log(sumIndex2);
+
+//  console.log(sum2 - sumIndex2);
+
+
+
+// Classwork
+// const users = [1, 4, 5, 2, 6, 8, 10, 11, 15, 9, 9]
+// const arr = [4, 8, 9, 1, 4, 9, 10, 11, 33, 21, 11]
+// arr.sort ((a, b)=> a - b)
+// console.log(arr);
+// arr.sort ((a, b)=> b - a)
+// console.log(arr);
+// arr.reverse()
+// console.log(arr);
+// const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// console.log(sum);
+// console.log(arr.find(item => item == 9));
+// console.log(arr.filter(item => item == 4));
+// console.log(arr.findIndex(item => item == 10));
+// const sum = arr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+// console.log(sum);
--- homework/script.js
+++ homework/script.js
@@ -1,1 +1,39 @@
-let userName = 'Xeyal'
+// const arr1 = [1, 2, 3]
+// const arr2 = [4, 5, 6]
+// console.log(arr1.concat (arr2));
+
+// const str1 = 'salam'
+// const str2 = 'sagol'
+// if (console.logstr1.startsWith ('s'))  ==  console.log(str2.startsWith 's') 
+// const sentence = 'bunu bolene halaldi'
+// console.log(sentence.endsWith ("!"));
+// console.log(sentence.includes ("bolene"));
+// console.log(sentence.replaceAll (' ' , ''));
+const car = {
+    name: 'MaseratiGranTurismoMC',
+    Hp: 450,
+    topSpeed:301,
+    price: 135000,
+    car1: { 
+
+        weith: 1800,
+        year: 2013,
+        oilCapacity: '90L'
+    }
+}
+
+console.log(car);
+const user = {
+    firstName: 'xeyal',
+    lastName: 'imanov',
+    age: 23,
+    City:'Baku',
+    country: 'Azerbaycan',
+    street: 'Nizami23'  }
+
+
+user.firstName = 'Dasqin'
+user.lastName = 'Rehimov'
+user.age = 16
+user.country = 'Hollandiya'
+
